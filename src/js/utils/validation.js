f58f443c03562a9837af95e21e79c364c8e039c0
won junho <wnsgh1993@gmail.com>
2022-07-24 18:27:04 +0900

    validation name 변경

--- src/js/utils/validation.js
+++ src/js/utils/validation.js
@@ -1,3 +1,3 @@
 import {PRICE_FOR_ONE} from '../const/const.js'
 
-export const validation = (target) => target % PRICE_FOR_ONE === 0;
+export const validationFor1000Unit = (target) => target % PRICE_FOR_ONE === 0;
