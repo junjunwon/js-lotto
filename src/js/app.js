60da91f6d8e23b2530c87485dfa4c721af054f52
won junho <wnsgh1993@gmail.com>
2022-07-24 18:28:54 +0900

    setLottoTicketAndView 별도 함수로 추출

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import Component from "./core/Component.js";
 import InputValue from "./components/InputValue.js";
 import ItemToggle from "./components/ItemToggle.js";
-import {validation} from "./utils/validation.js"
-import {generateRandom} from "./utils/generateRandom.js"
-import {PRICE_FOR_ONE} from './const/const.js'
-import {resetToggleBtn, displayLottoLabel, displayToggleBtn, lottoHidden, generateLottoTicket, removeAllLottoTickets} from './view/view.js'
+import {validationFor1000Unit} from "./utils/validation.js";
+import {generateLottoTicket} from "./utils/generate.js";
+import {PRICE_FOR_ONE} from './const/const.js';
+import {resetToggleBtn, displayLottoLabel, displayToggleBtn, lottoHidden, generateLottoTicketView, removeAllLottoTickets} from './view/view.js';
 
 export default class App extends Component{
 
@@ -22,27 +22,31 @@ export default class App extends Component{
 		const $inputMoneyForm = this.$target.querySelector('#input-money-form')
 		const $lottoToggleBtn = document.querySelector('.lotto-numbers-toggle-button')
 
+		/**
+		 * @todo : 별도의 함수로 빼서 addEventListener에 함수 등록하기.
+		 */
 		$inputMoneyForm.addEventListener('submit', event => {
 			event.preventDefault();
-			const $inputValue = event.target['inputMoney'].value
-			const isUnit1000 = validation(Number($inputValue))
+			/**
+			 * @todo : 값을 입력하는 부분 분리
+			 */
+			const $inputValue = event.target['inputMoney'].valueAsNumber;
+			const isUnit1000 = validationFor1000Unit($inputValue);
 			if(!isUnit1000) {
-				alert('1000원 단위로 입력해주세요.')
+				alert('1000원 단위로 입력해주세요.');
 				return;
 			}
+
 			if(this.$state.lottoList.length > 0 ) {
-				this.initLottoTickets()
+				this.initLottoTickets();
 			}
 			
-			this.setLottoCntInputValue(Number($inputValue))
-			displayLottoLabel(this.$state.lottoCnt)
-			lottoHidden(false)
-			for(let i = 0; i < this.$state.lottoCnt; i++) {
-				const lottoNums = generateRandom()
-				generateLottoTicket(lottoNums)
-				this.$state.lottoList.push(lottoNums)
-			}
-			
+			this.setLottoCntInputValue($inputValue);
+			displayLottoLabel(this.$state.lottoCnt);
+			lottoHidden(false);
+
+			//generate lottoTicket and View
+			this.setLottoTicketAndView();			
 		})
 
 		new ItemToggle($lottoToggleBtn, {
@@ -51,19 +55,30 @@ export default class App extends Component{
 	}
 
 	setLottoCntInputValue (inputValue) {
-		const lottoCnt = inputValue / PRICE_FOR_ONE
-		this.setState({ lottoCnt : lottoCnt }) 
-		this.setState({ inputValue: inputValue }) 
+		const lottoCnt = inputValue / PRICE_FOR_ONE;
+		this.setState({ lottoCnt : lottoCnt });
+		this.setState({ inputValue: inputValue });
 	}
 
 	setItemToggle (target) {
-		const setToggle = !target.checked
-		this.setState({lottoToggle: setToggle})
-		displayToggleBtn(this.$state)
+		const setToggle = !target.checked;
+		this.setState({lottoToggle: setToggle});
+		displayToggleBtn(this.$state);
 	}
 	
 	initLottoTickets () {
-		removeAllLottoTickets()
-		resetToggleBtn()
+		removeAllLottoTickets();
+		resetToggleBtn();
+	}
+
+	setLottoTicketAndView() {
+		let i = 0;
+		while(i < this.$state.lottoCnt) {
+			const lottoNums = generateLottoTicket();
+			generateLottoTicketView(lottoNums);
+			this.$state.lottoList.push(lottoNums);
+			i++;
+		}
+		
 	}
 } 
